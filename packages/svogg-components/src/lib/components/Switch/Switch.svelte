478a5c476fd3ea30464e2a8b2612ec5cf9f398f3
MikaelJohnAndersson <mickepunktjohn@gmail.com>
2023-11-23 18:05:38 +0100

    Create Switch component

---
+++ packages/svogg-components/src/lib/components/Switch/Switch.svelte
@@ -0,0 +1,28 @@
+<script>
+	/**
+	 * Checked state
+	 * @type {boolean}
+	 */
+	export let checked = false;
+
+	function toggle() {
+		checked = !checked;
+	}
+
+	/** @param {KeyboardEvent} ev */
+	function handleKeyDown(ev) {
+		if (ev.code == 'Enter' || ev.code == 'Space') toggle();
+	}
+</script>
+
+<div
+	role="switch"
+	class="svogg-switch"
+	aria-checked={checked}
+	tabindex="0"
+	class:checked
+	on:click={toggle}
+	on:keydown={handleKeyDown}
+>
+	<span class="thumb" />
+</div>
