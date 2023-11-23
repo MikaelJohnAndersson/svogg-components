af7ffbedda4eb39a4b948e5bfc2a133eb130ed22
MikaelJohnAndersson <mickepunktjohn@gmail.com>
2023-11-23 18:06:18 +0100

    docs: add switch

---
+++ sites/svogg-components.dev/src/routes/components/switch/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Switch } from 'svogg-components';
+	import sveld from 'svogg-components/components/Switch/Switch.svelte?raw&sveld';
+	import { CodeBlock, DocsShell, DocsType } from '$lib/components';
+	import type { DocsShellSettings } from '$lib/components';
+
+	const settings: DocsShellSettings = {
+		type: DocsType.Component,
+		title: 'Switch',
+		components: [{ name: 'Switch', sveld }],
+		imports: ['Switch'],
+		classes: [
+			['.svogg-switch', 'Applied to outer element'],
+			['.thumb', 'Applied to inner element'],
+			['.checked', 'Applied to outer element when checked']
+		],
+		keyboard: [[`<kbd>Enter or Space</kbd>`, ['Toggles checked state']]]
+	};
+
+	let checked = false;
+</script>
+
+<DocsShell {settings}>
+	<svelte:fragment slot="description">
+		<p>Toggle switch.</p>
+	</svelte:fragment>
+	<svelte:fragment slot="demo">
+		<Switch bind:checked />
+		<span class="py-2">
+			I am {checked ? '' : 'NOT'} checked!
+		</span>
+	</svelte:fragment>
+	<svelte:fragment slot="code">
+		<CodeBlock code={`let checked = false;`} language="typescript" />
+		<CodeBlock
+			code={`
+        <Switch bind:checked />
+        <span class="py-2">
+          I am {checked ? '' : 'NOT'} checked!
+        </span>
+      `}
+		/>
+	</svelte:fragment>
+</DocsShell>
